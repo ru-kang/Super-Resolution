064d52c408eafb6c8fa475b74f323ab6352060d6
藍光玄 <star012014@gmail.com>
2024-01-10 14:42:48 +0800

    update Discriminator.py

--- Discriminator.py
+++ Discriminator.py
@@ -1,28 +1,27 @@
 import tensorflow as tf
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, LeakyReLU, Dense, Flatten, AveragePooling2D
+from tensorflow.keras.layers import Input, Model, Conv2D, LeakyReLU, Dense, Flatten, BatchNormalization
 
-def build_discriminator(input_shape):
-    model = Sequential()
+def DownSampling(input_, unit, kernel_size, strides=1, bn=True):
+    x = Conv2D(unit, kernel_size=kernel_size, strides=strides, padding='same')(input)
+    if bn:
+        x = BatchNormalization(momentum=0.8)(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    return x
 
-    model.add(Conv2D(128, (3, 3), strides=(2, 2), padding='same', input_shape=input_shape))
-    model.add(LeakyReLU(alpha=0.2))
 
-    model.add(Conv2D(256, (3, 3), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
-    
-    model.add(Conv2D(512, (3, 3), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
-    
-    model.add(Conv2D(1024, (3, 3), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
+def build_discriminator(inputshape):
+    input = Input(inputshape)
+    model = DownSampling(input, unit= 64, kernel_size=3, bn=False)
 
-    model.add(AveragePooling2D())
+    model = DownSampling(model, unit=128, kernel_size=3, strides=2)
+    model = DownSampling(model, unit=256, kernel_size=3, strides=2)
+    feature = DownSampling(model, unit=512, kernelsize=3, strides=2)
 
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
+    model = Flatten()(feature)
+    model = LeakyReLU(alpha=0.2)(model)
+    output = Dense(1, activation='sigmoid')(model)
+    model = Model(inputs=input, outputs=output, name='discriminator')
 
-    return model
+    fn_model = Model(inputs=input, outputs=feature, name='fn')
 
-input_shape = (256, 256, 3)
-discriminator = build_discriminator(input_shape)
+    return model, fn_model
